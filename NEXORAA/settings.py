0c6da82abbd82d36443e5793f404f1d22921e49a
Ayoade, temitope Jeremiah <61752057+jerrytope@users.noreply.github.com>
2026-03-13 14:21:02 +0100

    yes

--- NEXORAA/settings.py
+++ NEXORAA/settings.py
@@ -124,11 +124,16 @@ import os
 #     )
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 from pathlib import Path
